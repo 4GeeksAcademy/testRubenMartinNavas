91b98f7b824bd6f7584f9b65c4cf6b1c82776291
Mnrubn <126007823+Mnrubn@users.noreply.github.com>
2025-09-01 18:59:59 +0000

    corregido

--- pruebas.test.js
+++ pruebas.test.js
@@ -2,19 +2,17 @@ const { fromEuroToDollar, fromDollarToYen, fromYenToPound, } = require('./app.js
 
 
 
-test("One euro should be 1.07 dollars", function () {
-    const dollars = fromEuroToDollar(3.5);
-    const expected = 3.5 * 1.07;
+test("3.5 euros should be 3.745 dollars", function () {
     expect(fromEuroToDollar(3.5)).toBe(3.745);
 });
 
 
-test("3.5 Dollar should be 511.91 Yen", function () {
+test("3.5 Dollars should be 511.91 Yen", function () {
     expect(fromDollarToYen(3.5)).toBeCloseTo(511.9158);
 });
 
 
-test("3.5 Yen should be  Pound 0.01945", function () {
+test("3.5 Yens should be  Pound 0.01945", function () {
     expect(fromYenToPound(3.5)).toBeCloseTo(0.01945);
 });
 
